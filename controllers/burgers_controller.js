db0a3361c28600e88d918e0f35dc636a159e3eb4
kobiihw <54731242+kobiihw@users.noreply.github.com>
2020-04-08 20:33:47 -0500

    finally finished

---
+++ controllers/burgers_controller.js
@@ -0,0 +1,41 @@
+var express = require("express");
+var router = express.Router();
+var burger = require("../models/burger.js");
+
+router.get("/", function (req, res) {
+    burger.all(function (data) {
+        var bgrObject = {
+            burgers: data
+        };
+        console.log("ROUTE ", bgrObject);
+        res.render("index", bgrObject);
+    });
+});
+
+router.post("/api/burgers", function (req, res) {
+    burger.create([
+        "burger_name", "devoured"
+    ], [
+        req.body.name, req.body.devoured
+    ], function (result) {
+        res.json({ id: result.insertId });
+    });
+});
+
+router.put("/api/burgers/:id", function (req, res) {
+    var condition = "id = " + req.params.id;
+
+    console.log("condition", condition);
+
+    burger.update({
+        devoured: req.body.devoured
+    }, condition, function (result) {
+        if (result.changedRows == 0) {
+            return res.status(404).end();
+        } else {
+            res.status(200).end();
+        }
+    });
+});
+
+module.exports = router;
